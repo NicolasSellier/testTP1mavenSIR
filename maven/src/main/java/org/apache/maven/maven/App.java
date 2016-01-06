692bf2736c5274c8a86f6a44164e49f9b1da0c06
13010866 <13010866@e010m01.istic.univ-rennes1.fr>
2016-01-06 18:00:29 +0100

    ertert

---
+++ maven/src/main/java/org/apache/maven/maven/App.java
@@ -0,0 +1,20 @@
+package org.apache.maven.maven;
+
+/**
+ *
+ * @author NOUS
+ *
+ */
+public class App {
+	/**
+	 *@param args
+	 */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public void methodeMorte(){
+    	int i = 0;
+    	i++;
+    }
+}
